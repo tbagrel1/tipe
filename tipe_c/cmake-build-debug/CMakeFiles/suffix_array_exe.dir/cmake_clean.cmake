25f9156faf6b04f3e131c40b465050ff6d218ec8
Thomas BAGREL <tomsb07@gmail.com>
2018-01-27 19:58:09 +0100

    excluded folders updated

---
+++ tipe_c/cmake-build-debug/CMakeFiles/suffix_array_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/suffix_array_exe.dir/suffix_array/main_suffix_array.c.o"
+  "CMakeFiles/suffix_array_exe.dir/suffix_array/suffix_array.c.o"
+  "CMakeFiles/suffix_array_exe.dir/suffix_array/clist.c.o"
+  "suffix_array_exe.pdb"
+  "suffix_array_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/suffix_array_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tipe_c/cmake-build-release/CMakeFiles/bwt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bwt.dir/bwt/bwt_2.c.o"
+  "CMakeFiles/bwt.dir/bwt/bwt_2_launcher.c.o"
+  "bwt.pdb"
+  "bwt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bwt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
